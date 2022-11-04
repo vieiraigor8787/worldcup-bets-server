f37ae9c991a346f2bae598ad1e9ca93f4b2af353
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2022-11-04 11:54:53 -0300

    verify if user is participating of poll

--- src/routes/guesses.ts
+++ src/routes/guesses.ts
@@ -5,20 +5,52 @@ import { authenticate } from '../plugins/authenticate';
 
 export async function guessRoutes(fastify: FastifyInstance) {
   fastify.get('/guesses/count', async () => {
-    const count = await prisma.guess.count()
+    const count = await prisma.guess.count();
 
-    return { count }
+    return { count };
   });
 
   //dentro de bolões, achar um bolão específico, onde existem vários jogos e dentro de jogos, achar um jogo específico onde o participante ira criar um palpite
-  fastify.post('/bets/:betId/games/:gameId/guesses', {
-    onRequest: [authenticate],
-  }, async (req, reply) => {
-    const createGuessParams = z.object({
-      betId: z.string(),
-      gameId: z.string(),
-    });
+  fastify.post(
+    '/bets/:betId/games/:gameId/guesses',
+    {
+      onRequest: [authenticate],
+    },
+    async (req, reply) => {
+      const createGuessParams = z.object({
+        betId: z.string(),
+        gameId: z.string(),
+      });
 
-    const { betId, gameId } = createGuessParams.parse(req.params)
-  })  
+      const createGuessBody = z.object({
+        firstCountryPoints: z.number(),
+        secondCountryPoints: z.number(),
+      });
+
+      const { betId, gameId } = createGuessParams.parse(req.params);
+      const { firstCountryPoints, secondCountryPoints } = createGuessBody.parse(
+        req.body
+      );
+
+      // usuário pertence ao bolão
+      const participant = await prisma.participant.findUnique({
+        where: {
+          userId_betId: {
+            betId,
+            userId: req.user.sub,
+          },
+        },
+      });
+
+      //se o usuário não faz parte do bolão
+      if (!participant) {
+        return reply.status(400).send({
+          message: 'Você não está permitido criar um palpite para este bolão',
+        });
+      }
+
+
+      return { firstCountryPoints, secondCountryPoints, gameId, betId };
+    }
+  );
 }
