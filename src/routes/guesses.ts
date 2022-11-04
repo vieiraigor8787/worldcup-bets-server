7cea09e2ca7bd452af3b509df1a2a0a7de4abb3f
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2022-11-04 11:40:32 -0300

    create route to guesses from a participant [confi]

--- src/routes/guesses.ts
+++ src/routes/guesses.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
 import { prisma } from '../lib/prisma';
+import { authenticate } from '../plugins/authenticate';
 
 export async function guessRoutes(fastify: FastifyInstance) {
   fastify.get('/guesses/count', async () => {
@@ -7,4 +9,16 @@ export async function guessRoutes(fastify: FastifyInstance) {
 
     return { count }
   });
+
+  //dentro de bolões, achar um bolão específico, onde existem vários jogos e dentro de jogos, achar um jogo específico onde o participante ira criar um palpite
+  fastify.post('/bets/:betId/games/:gameId/guesses', {
+    onRequest: [authenticate],
+  }, async (req, reply) => {
+    const createGuessParams = z.object({
+      betId: z.string(),
+      gameId: z.string(),
+    });
+
+    const { betId, gameId } = createGuessParams.parse(req.params)
+  })  
 }
