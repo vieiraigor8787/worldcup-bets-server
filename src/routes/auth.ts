2976160c738a3a9f7bb48a2219b358f8d91553ef
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2022-11-04 10:06:53 -0300

    refatoring auth plugin

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,16 @@
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { prisma } from '../lib/prisma';
+import { authenticate } from '../plugins/authenticate';
 
 export async function authRoutes(fastify: FastifyInstance) {
-  fastify.get('/me', async (req) => {
-    await req.jwtVerify()
-
-    return { user: req.user}
-  })
+  fastify.get('/me', {
+    onRequest: [authenticate]
+  },
+    async (request) => {
+      return { user: request.user }
+    }
+  )
 
   fastify.post('/users', async (request) => {
     const createUserBody = z.object({
